2d963e44858515c0e6c49023e478f125922f2eee
Agaspar11 <agasparadrianlatayada@gmail.com>
2023-06-30 16:36:48 +0800

    Fix the search

--- crypto.js
+++ crypto.js
@@ -6,53 +6,49 @@ const listElement = document.getElementById('cryptocurrency-list');
 const loadingIndicator = document.getElementById('loading-indicator');
 const endNotification = document.getElementById('end-notification');
 
-const fetchCryptocurrencyRates = (page) => {
+const fetchCryptocurrencyRates = async (page) => {
   loading = true;
   loadingIndicator.innerText = 'Loading...';
 
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const rates = data.rates;
-      const cryptocurrencies = Object.keys(rates);
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const rates = data.rates;
+    const cryptocurrencies = Object.keys(rates);
 
-      if (cryptocurrencies.length === 0) {
-        endOfRecords = true;
-        endNotification.innerText = 'No more records to show.';
-      } else {
-        cryptocurrencies.forEach((crypto) => {
-          const rate = rates[crypto].value;
-          const unit = rates[crypto].unit;
+    if (cryptocurrencies.length === 0) {
+      endOfRecords = true;
+      endNotification.innerText = 'No more records to show.';
+    } else {
+      cryptocurrencies.forEach((crypto) => {
+        const rate = rates[crypto].value;
+        const unit = rates[crypto].unit;
+        const cryptoElement = document.createElement('div');
+        cryptoElement.classList.add('cryptocurrency');
+        cryptoElement.innerHTML = `
+          <img src="${rates[crypto].icon}">
+          <div class="details">
+            <div class="rate"><span class="label">Rate:</span> ${rate}</div>
+            <div class="name"><span class="label">Crypto name:</span> ${rates[crypto].name}</div>
+            <div class="unit"><span class="label">Crypto unit:</span> ${unit}</div>
+          </div>`;
+        listElement.appendChild(cryptoElement);
+      });
 
-          const cryptoElement = document.createElement('div');
-          cryptoElement.classList.add('cryptocurrency');
-          cryptoElement.innerHTML = `
-            <div class="icon">
-              <img src="#">
-            </div>
-            <div class="details">
-              <div class="rate"><span class="label">Rate:</span> ${rate}</div>
-              <div class="name"><span class="label">Crypto name:</span> ${rates[crypto].name}</div>
-              <div class="unit"><span class="label">Crypto unit:</span> ${unit}</div>
-            </div>`;
-          listElement.appendChild(cryptoElement);
-        });
+      page++;
+    }
 
-        page++;
-      }
-
-      loading = false;
-      loadingIndicator.innerText = '';
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-      loading = false;
-      loadingIndicator.innerText = '';
-    });
+    loading = false;
+    loadingIndicator.innerText = '';
+  } catch (error) {
+    console.log('Error:', error);
+    loading = false;
+    loadingIndicator.innerText = '';
+  }
 };
 
 const lazyLoad = () => {
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
     if (!loading && !endOfRecords) {
       fetchCryptocurrencyRates(page);
     }
@@ -85,22 +81,24 @@ function filterCryptocurrencies(query) {
     .then((data) => {
       const rates = data.rates;
       const cryptocurrencies = Object.keys(rates);
-      cryptocurrencies.forEach((crypto) => {
-        const rate = rates[crypto].value;
-        const unit = rates[crypto].unit;
 
-        const cryptoElement = document.createElement('div');
-        cryptoElement.classList.add('cryptocurrency');
-        cryptoElement.innerHTML = `
-          <div class="icon">
-            <img src="#">
-          </div>
-          <div class="details">
-            <div class="rate"><span class="label">Rate:</span> ${rate}</div>
-            <div class="name"><span class="label">Crypto name:</span> ${rates[crypto].name}</div>
-            <div class="unit"><span class="label">Crypto unit:</span> ${unit}</div>
-          </div>`;
-        listElement.appendChild(cryptoElement);
+      cryptocurrencies.forEach((crypto) => {
+        const cryptoName = rates[crypto].name.toLowerCase();
+
+        if (cryptoName.includes(query)) {
+          const rate = rates[crypto].value;
+          const unit = rates[crypto].unit;
+          const cryptoElement = document.createElement('div');
+          cryptoElement.classList.add('cryptocurrency');
+          cryptoElement.innerHTML = `
+            <img src="${rates[crypto].icon}">
+            <div class="details">
+              <div class="rate"><span class="label">Rate:</span> ${rate}</div>
+              <div class="name"><span class="label">Crypto name:</span> ${rates[crypto].name}</div>
+              <div class="unit"><span class="label">Crypto unit:</span> ${unit}</div>
+            </div>`;
+          listElement.appendChild(cryptoElement);
+        }
       });
     })
     .catch((error) => {
